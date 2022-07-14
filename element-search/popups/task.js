f8f18d3e7f865e435f03db6312c225949374818f
Dmitry-Gavrilov <91532052+Dmitry-Gavrilov@users.noreply.github.com>
2022-07-14 22:01:23 +1000

    2

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,21 @@
 const modalMain = document.getElementById('modal_main');
-const modalClose = Array.from(document.getElementsByClassName('modal__close'));
 const modalSuccess = document.getElementById('modal_success');
-const showSuccess = modalMain.getElementsByClassName('show-success');
+const btnX = Array.from(document.getElementsByClassName('modal__close modal__close_times'));
+const btnRed = Array.from(document.getElementsByClassName('btn btn_danger modal__close show-success'));
 
 modalMain.className = "modal modal_active";
 
-for (let i = 0; i < modalClose.length; i++) {
-    modalClose[i].onclick = () => {
-      modalMain.className = 'modal';
-      modalSuccess.className = 'modal';
-    }
-}
+for (let i = 0; i < btnRed.length; i++) {
+ btnRed[i].addEventListener('click', () => {
+   modalMain.className = 'modal';
+   modalSuccess.className = 'modal modal_active';
+ })
+}  
+  for (let i = 0; i < btnX.length; i++) {
+  btnX[i].addEventListener('click', () => {
+    modalMain.className = 'modal';
+    modalSuccess.className = 'modal';
+   })
+ }
 
-for (let i = 0; i < showSuccess.length; i++) {
-    showSuccess[i].onclick = () => {
-        modalSuccess.className = "modal modal_active";
-    }
-}
-
+ 
